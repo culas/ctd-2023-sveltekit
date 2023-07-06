407305d72065c889f190c489a79dacd6c0a8ecc2
Lucas Schnüriger <lucas.schnueriger@me.com>
2023-07-06 19:13:15 +0200

    Fix load event type from changing page to layout

--- src/routes/talks/+layout.server.ts
+++ src/routes/talks/+layout.server.ts
@@ -1,7 +1,7 @@
 import type { Talk } from '$lib/talk/talk.model';
-import type { PageServerLoadEvent } from './$types';
+import type { LayoutServerLoadEvent } from './$types';
 
-export async function load({ fetch }: PageServerLoadEvent) {
+export async function load({ fetch }: LayoutServerLoadEvent) {
 	const res = await fetch('http://localhost:3000/items?_expand=room');
 	const talks: Talk[] = await res.json();
 	return { talks };
