4a4dd6325bf20e2969dc40f5d4f0554da7f32f89
Lucas Schnüriger <lucas.schnueriger@me.com>
2023-07-09 20:58:57 +0200

    Use mock data instead of data fetching from API

--- src/routes/contributors/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoadEvent } from './$types';
-import type { Contributor } from '$lib/contributor/contributor.model';
-import { API_URL } from '$env/static/private';
-
-export async function load({ fetch }: PageServerLoadEvent) {
-	const res = await fetch(`${API_URL}/contributors`);
-	const contributors: Contributor[] = await res.json();
-	return { contributors };
-}
--- src/routes/contributors/+page.svelte
+++ src/routes/contributors/+page.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
-  import type { PageData } from "./$types";
   import ContributorTile from "$lib/contributor/ContributorTile.svelte";
   import PageTitle from "$lib/shared/PageTitle.svelte";
+  import type { Contributor } from "$lib/contributor/contributor.model";
 
-  export let data: PageData;
+  export let contributors: Contributor[] = [
+    {
+      name: "Lucas Schnüriger",
+      subtitle: "Expert Software Engineer, Schlieren",
+      teaser: {
+        sizes: {
+          preview: "https://images.lineupr.com/034/22596b40944ca7e523a4d83e0cc9ecdd/7e5d760646.png",
+          small: "https://images.lineupr.com/034/22596b40944ca7e523a4d83e0cc9ecdd/c5916d7875.png",
+          original: "https://images.lineupr.com/034/22596b40944ca7e523a4d83e0cc9ecdd/original.png"
+        }
+      }
+    }
+  ];
 </script>
 
 <PageTitle>Lineup</PageTitle>
 <div class="px-5 overflow-y-auto">
-  {#each data.contributors as contributor}
+  {#each contributors as contributor}
     <ContributorTile {contributor} />
   {/each}
 </div>
